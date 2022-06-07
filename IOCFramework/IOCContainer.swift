146fded589c908880ccc1b7b8040b14535f17d39
Frank Saar <frank.saar@gmail.com>
2022-06-07 20:13:38 +0100

    cleanup

--- IOCFramework/IOCContainer.swift
+++ IOCFramework/IOCContainer.swift
@@ -29,7 +29,6 @@ public class IOCContainer {
     /// Registers Type with IOCContainer
     ///
     /// - Parameters:
-    ///     - type: type of class to register
     ///     - scope: scope of registration: .unique (default) or .shared
     ///     - block: closure to instantiate type when requested (see resolve)
     public func register<T>(scope : Scope = .unique,block: @escaping () -> T ) {
